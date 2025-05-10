4402233f31c151386a6d29d3451bad050d43e0cf
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-05-10 20:42:04 -0300

    feat: configurar acesso ao banco PostgreSQL com JPA e Docker Compose
    - Adicionadas dependências do Spring Data JPA e PostgreSQL no projeto.
    - Configurado o arquivo application.properties com as credenciais de acesso ao banco:
      - Nome do banco
      - Usuário
      - Senha
      - URL de conexão
    - Criado arquivo docker-compose.yml para subir o container do PostgreSQL com as configurações correspondentes.
    - Criada a entidade inicial (modelo de dados) que será utilizada como base para persistência no banco via JPA.

---
+++ src/main/java/com/mateus_lima/desafio/courses/course/entity/CourseEntity.java
@@ -0,0 +1,33 @@
+package com.mateus_lima.desafio.courses.course.entity;
+
+import com.mateus_lima.desafio.courses.course.enums.TypeCategory;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "Courses")
+public class CourseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @NotBlank(message = "O nome do curso é obrigatório.")
+    @Length(min = 3, max = 100, message = "O nome do curso deve ter entre 3 e 100 caracteres.")
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private TypeCategory category;
+
+    private Boolean active = true;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
